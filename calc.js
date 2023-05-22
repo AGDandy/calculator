2c74333109067e76ad36d4d329cc732a35a8bb76
AGDandy <jacob.bithorn@gmail.com>
2023-05-22 15:42:40 -0400

    create basic structure

---
+++ calc.js
@@ -0,0 +1,23 @@
+let num1;
+let operator;
+let num2;
+
+const add = function(x, y) {
+	let sum = x + y;
+    return sum;
+};
+
+const subtract = function(x, y) {
+	let diff = x - y;
+    return diff;
+};
+
+const multiply = function(x, y) {
+    let prod = x * y;
+    return product;
+};
+
+const divide = function(x, y) {
+    let quot = x / y;
+    return quot;
+}
