3c5c5ffb7a816dd1cf5d49b02ba7f28ade579e16
AGDandy <jacob.bithorn@gmail.com>
2023-05-23 15:02:15 -0400

    finish all core functionality

--- calc.js
+++ calc.js
@@ -5,11 +5,16 @@ let num2 = 0;
 
 let isStepOne = true;
 let isStepTwo = false;
-let isStepThree = false;
+let num2Entered = false;
+
+const disCont = document.getElementById("display");
+const display = document.createElement("div");
+disCont.appendChild(display);
 
 const numBtns = document.querySelectorAll(".digit");
 const opsBtns = document.querySelectorAll(".ops");
 const eval = document.querySelector("#equals");
+const aClr = document.querySelector("#clear");
 
 let opsArr = ["+", add, "-", subtract, "*", multiply, "/", divide];
 
@@ -18,6 +23,7 @@ function storeOps(input) {
         for (let i = 0; i < opsArr.length; i+=2) {
             if (input == opsArr[i]) {
                 nextFunction = opsArr[i+1];
+                operator = input;
                 break;
             }
             else {
@@ -27,16 +33,17 @@ function storeOps(input) {
     })
 }
 
-
+//what happens when digits are clicked
 numBtns.forEach((element) => {
     element.addEventListener("click", () => {
         if (isStepOne == true && isStepTwo == false) {
             num1 = parseInt((num1 + element.textContent), 10);
-            console.log(num1);
+            display.textContent = num1;
         }
         else if (isStepOne == false && isStepTwo == true) {
             num2 = parseInt((num2 + element.textContent), 10);
-            console.log(num2);
+            display.textContent = num1 + " " + operator + " " + num2;
+            num2Entered = true;
         }
         else {
             console.log("uhoh");
@@ -44,25 +51,45 @@ numBtns.forEach((element) => {
     })
 })
 
+//what happens when operators are clicked
 opsBtns.forEach((element) => {
     element.addEventListener("click", () => {
         if (isStepOne == true && isStepTwo == false) {
             storeOps(element.textContent);
+            display.textContent = num1 + " " + operator;
             isStepOne = false;
             isStepTwo = true;
-            /*console.log(nextFunction(2, 5));*/
         }
-        else {
-            console.log("yikes");
+        else if (num2Entered == true) {
+            num1 = nextFunction(num1, num2);
+            num2 = 0;
+            storeOps(element.textContent);
+            display.textContent = num1 + " " + operator;
         }
+        else {}
     })
 })
 
+//what happens when equals is clicked
 eval.addEventListener("click", () => {
-    if (isStepOne == false && isStepTwo == true) {
-        console.log(nextFunction(num1, num2));
+    if (isStepOne == false && isStepTwo == true && num2Entered == true) {
+        display.textContent = nextFunction(num1, num2);
+        reset();
     }
 })
+
+aClr.addEventListener("click", () => {
+    reset();
+    display.textContent = "";
+})
+
+function reset() {
+    num1 = 0;
+    num2 = 0;
+    isStepOne = true;
+    isStepTwo = false;
+    isnum2Entered = false;
+}
 
 function add(x, y) {
 	let sum = x + y;
@@ -80,6 +107,11 @@ function multiply(x, y) {
 };
 
 function divide(x, y) {
-    let quotient = x / y;
-    return quotient;
+    if (y != 0) {
+        let quotient = x / y;
+        return quotient;
+    }
+    else {
+        return "you can't do that";
+    } 
 }
